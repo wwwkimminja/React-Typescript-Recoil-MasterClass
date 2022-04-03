07b8df2e21cb95c5e6b050b6a543c8f1292afb8f
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-03 21:34:37 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,50 +1,21 @@
-import styled,{keyframes} from "styled-components";
+import styled from "styled-components";
 
+const Title = styled.h1`
+color : ${(props) => props.theme.textColor};
+`;
 const Wapper= styled.div`
  display: flex;
  height:100vh;
  width:100vw;
  justify-content:center;
  align-items:center;
-
-`;
-const rotationAnimation = keyframes`
-0%{
-transform:rotate(0deg);
-border-radius:0px:
-}
-50%{
-
-border-radius:100px;
-}
-100%{
-  transform:rotate(360deg);
-border-radius:0px:
-}
+background-color:${(props) => props.theme.backgroundColor}
 `;
 
-const Emoji = styled.div`
-font-size: 36px;
-`;
-const Box = styled.div`
-background-color: tomato;
-height:200px;
-width: 200px;
-animation: ${rotationAnimation} 1s linear infinite;
-display:flex;
-justify-content:center;
-align-items: center;
-${Emoji}:hover {
-  font-size:98px;
-}
-`;
 function App() {
   return (
 <Wapper>
-  <Box>
-    <Emoji >🤩</Emoji>
-  </Box>
-    <Emoji >👻</Emoji>
+<Title>Hello</Title>
 </Wapper>
   );
 }
