6af149b0c2c296a34e83725cf6d0f261aa8b8d81
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-03 20:47:50 +0900

    Animation and Pseudo Selectors

--- src/App.js
+++ src/App.js
@@ -1,30 +1,48 @@
-import styled from "styled-components";
+import styled,{keyframes} from "styled-components";
 
-const Father= styled.div`
+const Wapper= styled.div`
  display: flex;
 `;
+const rotationAnimation = keyframes`
+0%{
+transform:rotate(0deg);
+border-radius:0px:
+}
+50%{
 
-const Btn = styled.button`
- color: white;
- background-color: tomato;
- border:0;
- border-radius:15px;
-`
-
-const Input = styled.input.attrs({required: true ,minLength:10 })`
-background-color: tomato;
+border-radius:100px;
+}
+100%{
+  transform:rotate(360deg);
+border-radius:0px:
+}
 `;
+const Box = styled.div`
+background-color: tomato;
+height:200px;
+width: 200px;
+animation: ${rotationAnimation} 1s linear infinite;
+display:flex;
+justify-content:center;
+align-items: center;
+span{
+  font-size: 36px;
+  &:hover{
+    font-size: 50px;
+  }
+  &:active {
+    opacity:0;
+  }
+}
 
+`
 function App() {
   return (
- <Father as ="header">
-<Btn>Log in</Btn>
-<Btn as = "a" href="/">Log in</Btn>
-<Input/>
-<Input/>
-<Input/>
-<Input/>
-</Father>
+<Wapper>
+  <Box>
+    <span>🤩</span>
+  </Box>
+</Wapper>
   );
 }
 
