5a1a0650ba5e16b42d73eccc64c542b30b23728b
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-04 13:19:30 +0900

    Setting Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -12,7 +12,7 @@ const lightTheme ={
 }
 ReactDOM.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
     <App />
     </ThemeProvider>
   </React.StrictMode>,
