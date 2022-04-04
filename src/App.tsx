a1fd29bcefc222f02760616d0fec532665a266d3
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-04 15:01:33 +0900

    Forms

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import React, { useState } from "react";
 import styled from "styled-components";
-import Circle from "./Circle";
 
 function App() {
+  const [value,setValue] =useState("");
+  const onChange = (event: React.FormEvent<HTMLInputElement>)=> {
+    const {
+      currentTarget:{value},
+    } = event;
+    setValue(value);
+  }
+  const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
+    event.preventDefault();
+    console.log("hello",value);
+  };
+  
   return (
-<div>
-  <Circle bgColor="teal"/>
-  <Circle bgColor="tomato"/>
+    <div>
+<form onSubmit={onSubmit}>
+<input type="text" value={value} onChange ={onChange}placeholder="username"/>
+<button>Log in</button>
+</form>
 </div>
   );
+
 }
 
 export default App;
