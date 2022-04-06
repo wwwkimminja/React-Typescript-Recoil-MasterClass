5b49d19f2221ecb7906f0bea6745be568f2b6374
wwwkimminja <82824976+wwwkimminja@users.noreply.github.com>
2022-04-06 20:02:05 +0900

    Fecth CoinData
    async await

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router-dom";
 import styled from "styled-components";
 
@@ -31,13 +31,25 @@ function Coin() {
   const [loading,SetLoading] = useState(true);
     const {coinId} = useParams<RoutParams>();
     const {state} = useLocation<RouteState>();
+    const [info,setInfo] = useState({});
+    const [priceInfo,setPriceInfo] = useState({});
+
+    useEffect(()=>{
+      (async ()=> {
+        const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+        
+        const priceData = await (await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)).json();
+        setInfo(infoData);
+        setPriceInfo(priceData);
+      })();
+    },[])
     
   return  (
   <Container>
   <Header>
     <Title>{state?.name || "Loading..."}</Title>
   </Header>
-  {loading ? (<Loader>Loading...</Loader>):null}
+  {loading ? (<Loader>Loading...</Loader>):<span></span>}
   </Container>
 );
   }
